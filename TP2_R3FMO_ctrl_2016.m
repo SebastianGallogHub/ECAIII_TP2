fdf23fef4c9f979e4cd93d4f0bf14a7bb019f4ac
Sebastian <1sebastiangallo@gmail.com>
2021-09-16 23:28:51 -0300

    Corregir script principal para graficar algunos ensayos.

--- TP2_R3FMO_ctrl_2016.m
+++ TP2_R3FMO_ctrl_2016.m
@@ -1,147 +1,128 @@
-%% Declaro las variables de tiempo de la simulación
 
+%Formato de nombre de variable para graficar y comparar
+%<variables>_<alpha(rad)>[_<error(1 o 2)>]
+
+%Todos los ensayos se realizan con una carga {P=100W; Q=50VAr}
+
+%Ensayos:
+%1) alpha = p/3; anchoPulso = 5%; Comparar {Normal, t1=0, corto}
+%2) alpha = p/2; anchoPulso = 5%; Comparar {Normal, t1=0, corto}
+%3) alpha = p/3; anchoPulso = 40%; ¿Qué pasa en corto?
+%4) alpha = p/2; anchoPulso = 40%; ¿Qué pasa en corto?
+
+%TODO: Agregar gráfica y análisis de 3 y 4
+%TODO: Agregar gráfica de th1 (tensión y corriente de T1) para cada caso
+%TODO: Calcular valores eficaces
+
+clear all; close all; clc
 p = .01;%pi
-
 fase1 = 0;
 fase2 = fase1+2*p/3;
 fase3 = fase2+2*p/3;
-
-alfa = 0;
-theta = p/6 + alfa;
-anchoPulso = 5;
-Q = 100;
-C = 500e-5;
-tCorto = 0.2;
-
-t1 = fase1;
-t2 = fase2;
-t3 = fase3;
-
-%%
-%alfa = 0
-clc
-alpha = 0;
-theta = p/6+ alpha;
-
-t1 = fase1+theta;
-t2 = fase2+theta;
-t3 = fase3+theta;
-
-sim('TP2_R3FMO_ctrl_2016.slx')
-%Esto es igual que la rectificación con diodos. Las mismas gráficas y
-%conclusiones
-
-%%
-%Prueba
-clc
-alpha = p/6;
-theta = p/6+ alpha;
-
-t1 = fase1+theta;
-t2 = fase2+theta;
-t3 = fase3+theta;
-
-sim('TP2_R3FMO_ctrl_2016.slx')
-
-%Esto es lo que muestran en el apunte (theta = 60°). El tiempo que conduce
-%cada tiristor es el equivalente a un desarrollo de 120° en cualquier tipo
-%de carga.
-
-%En este caso la corriente en la carga es ininterrumpida.
-
-%En los casos de corriente ininterrumpida, el ángulo de apagado (theta2) 
-%de cada tiristor, coincide con el ángulo de encendido del tiristor 
-%siguiente y la transferencia de corriente de un tiristor al otro se 
-%produce por conmutación natural, al igual que con diodos.
-
-%Cuando el apagado (theta2) se produce antes de que encienda el tiristor 
-%siguiente, la corriente resulta interrumpida (pulsante).
-
-%%
-%alfa = pi/3
-clc
-alpha = p/3;
-theta = p/6+ alpha;
-
-t1 = fase1+theta;
-t2 = fase2+theta;
-t3 = fase3+theta;
-
-sim('TP2_R3FMO_ctrl_2016.slx')
-
-%Acá empezamos a ver corriente continua pero una tensión negativa en la
-%carga debido a la resistencia al cambio de corriente de la carga L que
-%provoca que el T1 siga conduciendo. Luego entra en conducción T2 debido a
-%la presencia del trigger en su gate. 
-
-%La corriente continua se sigue dando porque L no se alcanza a descargar
-%por completo.
-
-%%
-%alfa = pi/2
-clc
-alpha = p/2;
-theta = p/6+ alpha;
-
-t1 = fase1+theta;
-t2 = fase2+theta;
-t3 = fase3+theta;
-
-
-sim('TP2_R3FMO_ctrl_2016.slx')
-
-%%
-%alfa = 0; anchoPulso = 35
-clc
-anchoPulso = 40;
-alpha = p/3;
-theta = p/6+ alpha;
-
-t1 = fase1+theta;
-t2 = fase2+theta;
-t3 = fase3+theta;
-
-sim('TP2_R3FMO_ctrl_2016.slx')
-
-%%
-%alfa = 0; anchoPulso = 5; (potencia en la carga) Qm01 = 100VAr
-clc
-anchoPulso = 35;
-alpha = 0;
-theta = p/6+ alpha;
-
-t1 = fase1+theta;
-t2 = fase2+theta;
-t3 = fase3+theta;
-
-sim('TP2_R3FMO_ctrl_2016.slx')
-
-%%
-%alfa = 0; anchoPulso = 5; (potencia en la carga) Qm02 = 50VAr
-clc
 Q = 50;
-anchoPulso = 5;
-alpha = p/2;
-theta = p/6+ alpha;
+C = 500e-5;
 
-t1 = fase1+theta;
-t2 = fase2+theta;
-t3 = fase3+theta;
-
-sim('TP2_R3FMO_ctrl_2016.slx')
-
-%%
-%alfa = 0; anchoPulso = 5; (potencia en la carga) Qm02 = 50VAr
-clc
-Q = 100;
-%tCorto = 0.2;
-tCorto = 0.06;
-anchoPulso = 5;
+%% 1)Sin error; alpha = pi/3; anchoPulso = 5; 
 alpha = p/3;
 theta = p/6+ alpha;
-
+anchoPulso = 5; 
+tCorto = 0.2
 t1 = fase1+theta;
 t2 = fase2+theta;
 t3 = fase3+theta;
 
 sim('TP2_R3FMO_ctrl_2016.slx')
+tensionEnLaCarga_p_3= Vc;
+corrienteEnLaCarga_p_3 = icarga;
+tout_p_3 = tout;
+
+%% 1)A)Falla de SW; alpha = pi/3; anchoPulso = 5;
+t1 = 0;
+tCorto = 0.2;
+sim('TP2_R3FMO_ctrl_2016.slx')
+tensionEnLaCarga_p_3_1= Vc;
+corrienteEnLaCarga_p_3_1 = icarga;
+tout_p_3_1 = tout;
+
+%% 1)B)Error de corto; alpha = pi/3; anchoPulso = 5;
+t1 = fase1+theta;
+tCorto = 0.075;
+sim('TP2_R3FMO_ctrl_2016.slx')
+tensionEnLaCarga_p_3_2= Vc;
+corrienteEnLaCarga_p_3_2 = icarga;
+tout_p_3_2 = tout;
+
+%% Sin error; alpha = pi/2; anchoPulso = 5
+alpha = p/2;
+theta = p/6+ alpha;
+anchoPulso = 5; 
+tCorto = 0.2
+t1 = fase1+theta;
+t2 = fase2+theta;
+t3 = fase3+theta;
+
+sim('TP2_R3FMO_ctrl_2016.slx')
+tensionEnLaCarga_p_2 = Vc;
+corrienteEnLaCarga_p_2 = icarga;
+tout_p_2 = tout;
+
+%% 2)A)Falla de SW; alpha = pi/2; anchoPulso = 5
+t1 = 0;
+tCorto = 0.2;
+sim('TP2_R3FMO_ctrl_2016.slx')
+tensionEnLaCarga_p_2_1= Vc;
+corrienteEnLaCarga_p_2_1 = icarga;
+tout_p_2_1 = tout;
+
+%% 2)B)Error de Corto; alpha = pi/2; anchoPulso = 5
+t1 = fase1+theta;
+tCorto = 0.075;
+sim('TP2_R3FMO_ctrl_2016.slx')
+tensionEnLaCarga_p_2_2= Vc;
+corrienteEnLaCarga_p_2_2 = icarga;
+tout_p_2_2 = tout;
+
+%% 3)
+%TODO
+
+%% 4)
+%TODO
+
+%% 1)Gráficas
+%TODO: titulos y demás referencias
+figure;hold on; 
+title('Tension - Comparación con una falla de Software')
+plot(tout_p_3,tensionEnLaCarga_p_3, 'b--',tout_p_3_1,tensionEnLaCarga_p_3_1, 'r');grid on;
+
+figure;hold on 
+title('Corriente - Comparación con una falla de Software')
+plot(tout_p_3,corrienteEnLaCarga_p_3, 'b--',tout_p_3_1,corrienteEnLaCarga_p_3_1, 'r');grid on;
+
+figure;hold on; 
+title('Tension - Comparación con un corto en T3')
+plot(tout_p_3,tensionEnLaCarga_p_3, 'b--',tout_p_3_2,tensionEnLaCarga_p_3_2, 'r');grid on;
+
+figure;hold on 
+title('Corriente - Comparación con un corto en T3')
+plot(tout_p_3,corrienteEnLaCarga_p_3, 'b--',tout_p_3_2,corrienteEnLaCarga_p_3_2, 'r');grid on;
+
+%% 2)Gráficas
+figure;hold on; 
+title('Tension - Comparación con una falla de Software')
+plot(tout_p_2,tensionEnLaCarga_p_2, 'b--',tout_p_2_1,tensionEnLaCarga_p_2_1, 'r');grid on;
+
+figure;hold on 
+title('Corriente - Comparación con una falla de Software')
+plot(tout_p_2,corrienteEnLaCarga_p_2, 'b--',tout_p_2_1,corrienteEnLaCarga_p_2_1, 'r');grid on;
+
+figure;hold on; 
+title('Tension - Comparación con un corto en T3')
+plot(tout_p_2,tensionEnLaCarga_p_2, 'b--',tout_p_2_2,tensionEnLaCarga_p_2_2, 'r');grid on;
+
+figure;hold on 
+title('Corriente - Comparación con un corto en T3')
+plot(tout_p_2,corrienteEnLaCarga_p_2, 'b--',tout_p_2_2,corrienteEnLaCarga_p_2_2, 'r');grid on;
+
+%% 3)Gráficas
+
+%% 4)Gráficas
